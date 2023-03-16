fc179a426484cd6afbf9567dba1d014ce2e5c5ba
Lucía Pérez <100713036+Lucperrom@users.noreply.github.com>
2023-03-16 17:30:38 +0100

    validation.code

--- src/main/java/acme/entities/configuration/Configuration.java
+++ src/main/java/acme/entities/configuration/Configuration.java
@@ -15,9 +15,10 @@ public class Configuration extends AbstractEntity {
 	// Attributes -------------------------------------------------------------
 
 	@NotBlank
-	@Pattern(regexp = "^[A-Z]{3}$")
+	@Pattern(regexp = "^[A-Z]{3}$", message= "{validation.configuration.code1}")
 	protected String			defaultCurrency;
 
 	@NotBlank
+	@Pattern(regexp = "^([A-Z]{3}(,\b|\Z))+$", message="{validation.configuration.code2}")
 	protected String			acceptedCurrencies;
 }
